4f0d027de02e5e3899b3556fe8d5806d0d357281
Yasir Mohamed <yasir.mohamed@getyourguide.com>
2025-01-23 13:34:14 +0100

    Add basic tests

--- src/test/java/com/getourguide/interview/GetYourGuideApplicationTests.java
+++ src/test/java/com/getourguide/interview/GetYourGuideApplicationTests.java
@@ -1,13 +1,23 @@
 package com.getourguide.interview;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GetYourGuideApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mockMvc.perform(get("/activities"))
+			.andExpect(status().isOk());
 	}
-
 }
